e016334805217e2cccd4b69234a5414c15491e5c
JMN327 <173676271+JMN327@users.noreply.github.com>
2025-01-06 23:25:34 +0000

    Grid experiments

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ console.log(`Storage available: ${storageAvailable("localStorage")}`);
 
 let body = document.querySelector("body");
 
-//let pallet = addBasicElement("div", ["pallet"], body)
-/* addTileElement("cross", "red", pallet)
+/* let pallet = addBasicElement("div", ["pallet"], body)
+addTileElement("cross", "red", pallet)
 addTileElement("clover", "orange", pallet)
 addTileElement("diamond", "yellow", pallet)
 addTileElement("circle", "green", pallet)
 addTileElement("square", "blue", pallet)
 addTileElement("star", "purple", pallet) */
 
-let gridSize = 9;
+/* let gridSize = 9;
 let grid = addBasicElement("div", ["grid"], body);
 populateGrid();
 
@@ -54,7 +54,62 @@ function populateGrid() {
       addTileElement("circle", "purple", cell);
     }
   }
-}
+} */
 
-//grid-template-columns: repeat(var(--grid-size), 1fr);
-//grid-template-rows: repeat(var(--grid-size), 1fr);
+let outer = addBasicElement("div", ["outer"], body);
+let inner = addBasicElement("div", ["inner"], outer);
+let mouseY;
+let mouseX;
+let clickOffsetY;
+let clickOffsetX;
+
+let innerHeight = 1200;
+let innerWidth = 1800;
+let outerHeight = 800;
+let outerWidth = 800;
+
+inner.style.height = innerHeight + "px";
+inner.style.width = innerWidth + "px";
+outer.style.height = outerHeight + "px";
+outer.style.width = outerWidth + "px";
+
+let innerMouseDown = false;
+
+inner.addEventListener("dragstart", (event) => {
+  event.preventDefault();
+});
+
+inner.addEventListener("mousedown", (event) => {
+  if (event.buttons !== 1) {
+    return;
+  }
+  innerMouseDown = true;
+  clickOffsetY = event.clientY - inner.getBoundingClientRect().top;
+  clickOffsetX = event.clientX - inner.getBoundingClientRect().left;
+});
+
+document.body.addEventListener("mousemove", (event) => {
+  if (!innerMouseDown) {
+    return;
+  }
+  if (event.buttons !== 1) {
+    return;
+  }
+  let top = event.clientY - outer.getBoundingClientRect().top - clickOffsetY;
+  let bottom = top + innerHeight - outerHeight;
+  let left = event.clientX - outer.getBoundingClientRect().left - clickOffsetX;
+  let right = left + innerWidth - outerWidth;
+
+  if (top < 0 && bottom > 0) {
+    inner.style.top = top + "px";
+  }
+  if (left < 0 && right > 0) {
+    inner.style.left = left + "px";
+  }
+
+  console.log(left, right, top, bottom);
+});
+
+document.body.addEventListener("mouseup", (event) => {
+  innerMouseDown = false;
+});
