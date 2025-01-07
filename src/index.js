7a6050dfb99a4760d67cc5739f41e18c3d11c300
JMN327 <173676271+JMN327@users.noreply.github.com>
2025-01-07 15:05:01 +0000

    Grid experiments

--- src/index.js
+++ src/index.js
@@ -63,15 +63,25 @@ let mouseX;
 let clickOffsetY;
 let clickOffsetX;
 
-let innerHeight = 1200;
-let innerWidth = 1800;
 let outerHeight = 800;
 let outerWidth = 800;
-
-inner.style.height = innerHeight + "px";
-inner.style.width = innerWidth + "px";
 outer.style.height = outerHeight + "px";
 outer.style.width = outerWidth + "px";
+const innerHeight_Default = 1000;
+const innerWidth_Default = 1500;
+const aspectRatio = innerHeight_Default / innerWidth_Default;
+inner.style.height = innerHeight_Default + "px";
+inner.style.width = innerWidth_Default + "px";
+let zoomLevel = 0;
+const zoomLevelMax = 10;
+let zoomFactor = 10;
+const zoomAmountX = innerHeight_Default / zoomFactor;
+const zoomAmountY = innerWidth_Default / zoomFactor;
+let innerH_current = innerHeight_Default;
+let innerW_current = innerWidth_Default;
+
+inner.style.height = innerHeight_Default + "px";
+inner.style.width = innerWidth_Default + "px";
 
 let innerMouseDown = false;
 
@@ -95,21 +105,82 @@ document.body.addEventListener("mousemove", (event) => {
   if (event.buttons !== 1) {
     return;
   }
-  let top = event.clientY - outer.getBoundingClientRect().top - clickOffsetY;
-  let bottom = top + innerHeight - outerHeight;
-  let left = event.clientX - outer.getBoundingClientRect().left - clickOffsetX;
-  let right = left + innerWidth - outerWidth;
-
-  if (top < 0 && bottom > 0) {
-    inner.style.top = top + "px";
-  }
-  if (left < 0 && right > 0) {
-    inner.style.left = left + "px";
-  }
-
-  console.log(left, right, top, bottom);
+  moveInner(event);
 });
 
 document.body.addEventListener("mouseup", (event) => {
   innerMouseDown = false;
 });
+
+inner.addEventListener("wheel", (event) => zoom(event), { passive: false });
+
+function zoom(event) {
+  event.preventDefault();
+
+  if (event.deltaY < 0) {
+    zoomLevel++;
+    if (zoomLevel > zoomLevelMax) {
+      console.log(`max zoom level reached`);
+      zoomLevel--
+      return;
+    }
+    console.log("zooming in");
+  } else {
+    console.log("zooming out");
+    zoomLevel--;
+  }
+  console.log(`zoom level: ${zoomLevel}`);
+
+  let innerH_proposed = innerHeight_Default + zoomLevel * zoomAmountX;
+  let innerW_proposed = innerWidth_Default + zoomLevel * zoomAmountY;
+
+  /* if (innerH_proposed >= outerHeight && innerW_proposed >= outerWidth) {
+    innerH_current = innerH_proposed;
+    innerW_current = innerW_proposed;
+  } */
+
+  if (innerH_proposed < outerHeight) {
+    const difY = (innerH_proposed + zoomAmountX - outerHeight) / aspectRatio;
+    zoomLevel++;
+    innerH_current = outerHeight;
+    innerW_current = innerWidth_Default + zoomLevel * zoomAmountY + difY;
+  } else if (innerW_proposed < outerWidth) {
+    const difX = (innerW_proposed + zoomAmountY - outerWidth) * aspectRatio;
+    zoomLevel++;
+    innerH_current = innerHeight_Default + zoomLevel * zoomAmountX + difX;
+    innerW_current = outerWidth;
+  } else {
+    innerH_current = innerH_proposed;
+    innerW_current = innerW_proposed;
+  }
+
+  console.log(innerH_current, innerW_current, innerH_current / innerW_current);
+
+  inner.style.height = innerH_current + "px";
+  inner.style.width = innerW_current + "px";
+  //moveInner(event);
+}
+
+function moveInner(event) {
+  let top = event.clientY - outer.getBoundingClientRect().top - clickOffsetY;
+  let bottom = top + innerH_current - outerHeight;
+  let left = event.clientX - outer.getBoundingClientRect().left - clickOffsetX;
+  let right = left + innerW_current - outerWidth;
+
+  if (top < 0 && bottom > 0) {
+    inner.style.top = top + "px";
+  } else if (top >= 0) {
+    inner.style.top = "0px";
+  } else {
+    inner.style.top = outerHeight - innerH_current + "px";
+  }
+  if (left < 0 && right > 0) {
+    inner.style.left = left + "px";
+  } else if (left >= 0) {
+    inner.style.left = "0px";
+  } else {
+    inner.style.left = outerWidth - innerW_current + "px";
+  }
+
+  console.log(event.clientY, left, right, top, bottom);
+}
