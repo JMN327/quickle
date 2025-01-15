00836ea29cf5b189cfd96984c8fc81a87a11b7c5
JMN327 <173676271+JMN327@users.noreply.github.com>
2025-01-15 22:37:59 +0000

    More interface work

--- src/index.js
+++ src/index.js
@@ -13,13 +13,7 @@ console.log(`Session Storage available: ${storageAvailable("sessionStorage")}`);
 
 let body = document.querySelector("body");
 
-/* let pallet = addBasicElement("div", ["pallet"], body)
-addTileElement("cross", "red", pallet)
-addTileElement("clover", "orange", pallet)
-addTileElement("diamond", "yellow", pallet)
-addTileElement("circle", "green", pallet)
-addTileElement("square", "blue", pallet)
-addTileElement("star", "purple", pallet) */
+
 
 ///// outer /////
 let frame = addBasicElement("div", ["zpw"], body);
@@ -45,17 +39,12 @@ let zpw = zoomPanWindow(frame)
 zpw.setBounded(true)
 console.log(zpw.zoomScaleFactor)
 
-/* let rect = document.createElement("div")
-rect.style.backgroundColor = "greenyellow"
-rect.style.width = "100px"
-rect.style.height = "100px"
-rect.style.position = "absolute"
-rect.style.top = "100px"
+let pallet = addBasicElement("div", ["pallet"], body)
+addTileElement("cross", "red", pallet)
+addTileElement("clover", "orange", pallet)
+addTileElement("diamond", "yellow", pallet)
+addTileElement("circle", "green", pallet)
+addTileElement("square", "blue", pallet)
+addTileElement("star", "purple", pallet)
 
-let zpw = zoomPanWindow(frame)
-let zpw2 = zoomPanWindow(frame2)
-zpw2.setBounded(false)
-zpw2.setZoomLevelMax(20)
-zpw2.setZoomLevelMin(0)
-zpw2.setViewHeight(1200)
-zpw2.appendChild(rect) */
+zpw.appendChild(pallet)
