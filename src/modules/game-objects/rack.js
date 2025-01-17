155bd767f742bdfb0411fcebf6e107f7d26ea5f4
JMN327 <173676271+JMN327@users.noreply.github.com>
2025-01-17 17:17:38 +0000

    Start on rack object

--- src/modules/game-objects/rack.js
+++ src/modules/game-objects/rack.js
@@ -1,15 +1,23 @@
+import Bag from "./bag";
+
 export default function rack() {
+  let tiles = [];
 
-    // .tiles = []
-    // .fill = tiles =>bag.drawTiles(6 - tiles.length)
-    // .selection = tile number of interface selected tile
-    // .select = push tile onto selection
-    // .swapMode = bool activated if player wants to swap tiles
-    // .swapSelect = push tile onto swapSelection
-    // .swapSelection = [] of tile numbers for swapping
-    // .deselect = remove tile from selection
-    // .swap(selection[]) = bag.swapTiles(selection[])
+  function fill() {
+    let count = 6 - tiles.length;
+    tiles = tiles.splice(tiles.length, 0, ...bag.draw(count));
+  }
+  // .fill = tiles =>bag.drawTiles(6 - tiles.length)
+  // .selection = tile number of interface selected tile
+  // .select = push tile onto selection
+  // .swapMode = bool activated if player wants to swap tiles
+  // .swapSelect = push tile onto swapSelection
+  // .swapSelection = [] of tile numbers for swapping
+  // .deselect = remove tile from selection
+  // .swap(selection[]) = bag.swapTiles(selection[])
 
-
-
+  return {
+    tiles,
+    fill,
+  };
 }
