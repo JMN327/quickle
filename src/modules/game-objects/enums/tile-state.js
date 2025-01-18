c18e4bec556231242b7e269be20145a38381888f
jmn327 <173676271+JMN327@users.noreply.github.com>
2025-01-18 17:21:36 +0000

    Work on abstracting rack object

--- src/modules/game-objects/enums/tile-state.js
+++ src/modules/game-objects/enums/tile-state.js
@@ -2,6 +2,7 @@ export const tileState = Object.freeze({
   BAG: "bag",
   RACK: "rack",
   SELECTED: "selected",
+  MOVING: "moving",
   LOOSE: "loose",
   FIXED: "fixed",
 });
--- src/modules/game-objects/rack.js
+++ src/modules/game-objects/rack.js
@@ -2,14 +2,10 @@ import Bag from "./bag";
 import { tileState } from "./enums/tile-state";
 
 export default function Rack() {
-  let tiles = [
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-  ];
+
+  let tiles = new Array(6)
+  tiles.fill(undefined)
+  Object.seal(tiles)
 
   function select(i) {
     if (i > 5 || i < 0) {
@@ -25,7 +21,9 @@ export default function Rack() {
   }
   function deselectAll() {
     tiles.forEach((tile) => {
-      tile.state = tileState.RACK;
+      if (tile != undefined) {
+        tile.state = tileState.RACK;
+      }
     });
   }
   function xSelect(i) {
@@ -36,48 +34,89 @@ export default function Rack() {
     tiles[i].state = tileState.SELECTED;
   }
 
-  function remove() {}
-
   function rearrange(from, to) {
     if (from > 5 || from < 0 || to > 5 || to < 0) {
       throw new Error("Invalid indexes for rearrangement");
     }
-    tiles.splice(to, 0, tiles.splice(from, 1)[0]);
+    let gap = tiles[from]
+    tiles[from] = tiles[to]
+    tiles[to] = gap
+    //tiles.splice(to, 0, tiles.splice(from, 1)[0]);
+  }
+
+  function getSelection() {
+    let selected = [];
+    tiles.forEach((tile, index) => {
+      if (tile == undefined) {
+        return;
+      }
+      if ((tile.state == tileState.SELECTED)) {
+        selected.push(index);
+      }
+    });
+    return selected;
+  }
+
+  function getSpaces() {
+    let emptySpaces = [];
+    tiles.forEach((tile, index) => {
+      if (tile == undefined) {
+        emptySpaces.push(index);
+      }
+    });
+    console.table(emptySpaces);
+    return emptySpaces;
+  }
+
+  function addTiles(arr) {
+    if (!arr) {
+      throw new Error("No tiles passed for adding");
+    }
+    let emptySpaces = getSpaces();
+    if (arr.length > emptySpaces.length) {
+      throw new Error(
+        "Trying to pass more tiles to the rack than spaces available"
+      );
+    }
+    for (let i = 0; i < arr.length; i++) {
+      tiles.splice(emptySpaces[i],1,arr[i])
+      tiles[emptySpaces[i]].state = tileState.RACK
+    }
+  }
+
+  function removeSelection() {
+    let selectionArr = getSelection();
+    let removedTileArr = [];
+    if (!selectionArr) {
+      throw new Error("there are no selected tiles on the rack");
+    }
+    selectionArr.forEach((tileIndex) => {
+      tiles[tileIndex].state = tileState.MOVING;
+      removedTileArr.push(tiles.splice(tileIndex, 1, undefined)[0]);
+    });
+    return removedTileArr;
   }
 
   return {
     tiles,
+    addTiles,
+    removeSelection,
     rearrange,
     select,
     xSelect,
     deselectSingle,
     deselectAll,
 
-    get emptySpaces() {
-      let emptySpaces = [];
-      tiles.forEach((tile, index) => {
-        if (tile == undefined) {
-          emptySpaces.push(index);
-        }
-      });
-      console.table(emptySpaces);
-      return emptySpaces;
+    get spaces() {
+      return getSpaces();
+    },
+    get spaceCount() {
+      return getSpaces().length;
     },
 
     get selection() {
-      let selected = [];
-      tiles.forEach((tile, index) => {
-        if (tile == undefined) {
-          return;
-        }
-        if ((tile.state = tileState.SELECTED)) {
-          selected.push(index);
-        }
-      });
-      console.table(selected);
-      return selected;
+      return getSelection();
     },
-
   };
 }
 
@@ -95,16 +134,6 @@ export function TileManager() {
       //tiles[space] = ...bag.draw(1);
       slots[space].state = tileState.RACK;
     });
-  }
-
-  function playSelected() {
-    if (!selectedTilesIndexArr) {
-      throw new Error("there is no selected tile on the rack");
-    }
-    let tileToBePlayed = slots.splice(selectedTilesIndexArr, 1)[0];
-    slots.splice(selectedTilesIndexArr, 0, undefined);
-    tileToBePlayed.state = tileState.LOOSE;
-    return tileToBePlayed;
   }
 
   return {
