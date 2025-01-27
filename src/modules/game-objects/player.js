a938bcef047823d54b0405a19a205c6fd4e93310
jmn327 <173676271+JMN327@users.noreply.github.com>
2025-01-27 20:03:33 +0000

    start work on player object

--- src/modules/game-objects/player.js
+++ src/modules/game-objects/player.js
@@ -1,5 +1,6 @@
-export default function Player() {
+import Rack from "./rack";
 
+export default function Player() {
   // MOST OF THIS TO BE MOVED TO PLAYER MANAGER CONTROLLER
 
   // player handles the movement of tiles and owns a score sheet
@@ -9,53 +10,20 @@ export default function Player() {
   // validity for a given tile based on currently placed tiles
   // - so another board function needed
 
-    let players = [
-      {
-        name: "Player One",
-        rack: [],
-        score: 0,
-        index: 0,
-      },
-      {
-        name: "Player Two",
-        rack: [],
-        score: 0,
-        index: 1,
-      },
-    ];
+  let rack = Rack();
 
-    let startPlayer = Math.round(Math.random());
-    let activePlayer = players[startPlayer];
+  let score = {
+    turnScores: [],
+    accumulatedScores: [],
+    add: (newScore) => {
+      turnScores.push(newScore);
+      accumulatedScores.push(newScore + turnScores[turnScores.length - 1]);
+    },
+  };
 
-    const switchStartPlayer = () => {
-      startPlayer = startPlayer === players[0] ? players[1] : players[0];
-      activePlayer = startPlayer;
-    };
-
-    const getActivePlayer = () => activePlayer;
-
-    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-
-    const setPlayerNames = (playerOneName = "Player One" , playerTwoName = "Player Two" ) => {
-      players[0].name = playerOneName 
-      players[1].name = playerTwoName
-    };
-    const setPlayerMarks = (playerOneMark, playerTwoMark) => {
-      players[0].mark = playerOneMark;
-      players[1].mark = playerTwoMark;
-    };
-    const incrementScores = (playerIndex) => {
-      players[playerIndex].score++;
-    };
-    const resetScores = () => {
-      players[0].score = 0;
-      players[1].score = 0;
-    };
-    const getPlayers = () => players;
-
-    return {
-
-    };
-  }
+  return {
+    get rack() {
+      return rack;
+    },
+  };
+}
